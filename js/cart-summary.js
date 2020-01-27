e0659047149c2bdce5825ef7a56308f2a33ed1a1
Zengraph <nicozengraph@gmail.com>
2020-01-27 21:43:34 +0100

    update for total container amount to be updated
    the total amount was not updated, due to modifications in html ids in tpl files, now it works again on order-summary page.

--- js/cart-summary.js
+++ js/cart-summary.js
@@ -744,7 +744,7 @@ function updateCartSummary(json) {
     $('#total_product').html(formatCurrency(json.total_products, currencyFormat, currencySign, currencyBlank));
   else
     $('#total_product').html(formatCurrency(json.total_products_wt, currencyFormat, currencySign, currencyBlank));
-  $('#total_price').html(formatCurrency(json.total_price, currencyFormat, currencySign, currencyBlank));
+  $('#total_price_container').html(formatCurrency(json.total_price, currencyFormat, currencySign, currencyBlank));
   $('#total_price_without_tax').html(formatCurrency(json.total_price_without_tax, currencyFormat, currencySign, currencyBlank));
   $('#total_tax').html(formatCurrency(json.total_tax, currencyFormat, currencySign, currencyBlank));
 
